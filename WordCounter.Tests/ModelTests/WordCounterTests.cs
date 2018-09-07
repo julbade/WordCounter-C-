05ba76a5228be752182a81c7e1227241f1c5036e
julius bade <julbade21@gmail.com>
2018-09-07 15:14:19 -0700

    failed first test method

--- WordCounter.Tests/ModelTests/WordCounterTests.cs
+++ WordCounter.Tests/ModelTests/WordCounterTests.cs
@@ -11,14 +11,15 @@ namespace WordCounter.Tests
     public void countWords_NumberofWords()
         {
             //Arrange
-            RepeatCounter
-
+            string wordToFind = "sample";
+            string sentenceInput = "This sample is a sample hello";
+            int expectedValue = 2;
 
             //Act
-
-
+            int actualValue = RepeatCounter.countWords(wordToFind, sentenceInput);
 
             //Assert
+            Assert.AreEqual(expectedValue, actualValue);
         }
 
   }
