ddf2a328aacef9fc7304924fb501961ad33b07a4
julius bade <julbade21@gmail.com>
2018-09-07 11:07:47 -0700

    added initial test code

---
+++ WordCounter.Tests/ModelTests/WordCounterTests.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordCounter;
+using System.Collections.Generic;
+
+namespace WordCounter.Tests
+{
+  [TestClass]
+  public class WordCounterTest
+  {
+    int count = 0;
+    List<string> testList = new List<string> {"sample", "sample", "sample"};
+    WordCounter testCount = new WordCounter();
+
+    [TestMethod]
+    public void  WordCount_TestNumberofSample()
+    {
+      foreach(string x in WordCounter.WordCount(3))
+      {
+        if (x == "sample")
+        count ++;
+      }
+      Assert.AreEqual(1, count);
+    }
+
+  }
+}
---
+++ WordCounter/Models/WordCounter.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collection.Generic;
+
+namespace WordCounter
+{
+  public class RepeatCounter
+  {
+    private string 
+  }
+}
